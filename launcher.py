7591da2d0cd5cf6b5f464ad248a6a6c6786bab1c
aaznutii <76644651+aaznutii@users.noreply.github.com>
2022-04-14 20:43:06 +0400

    init less_5

--- launcher.py
+++ launcher.py
@@ -1,17 +1,20 @@
 
 import subprocess
-# from server import main
+
 process = []
 
-# print(f'python {sys.path[0]}\server.py')
+# Для запуска под виндовс пришлось поставить PyQt5 через консоль. Иначе не видел библиотеку.
+# Возможное решение на перспективу - Иначе прописать в самом server.py положение библиотек. ил pyinstaller
+# pyinstaller испытал в ветке py_to_exe. Повозился, но получилось.
 
+process.append(subprocess.Popen(f'python server.py'))
 while True:
     action = input('Выберите действие: q - выход , s - запустить сервер и клиенты, x - закрыть все окна:')
     if action == 'q':
         break
     elif action == 's':
         clients_count = int(input('Введите количество тестовых клиентов для запуска: '))
-        # process.append(subprocess.Popen(main()))
+
         # Запускаем клиентов:
         for i in range(clients_count):
             process.append(subprocess.Popen(f'python client.py -n test{i + 1}', creationflags=subprocess.CREATE_NEW_CONSOLE))
